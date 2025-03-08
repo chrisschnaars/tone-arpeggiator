144709deadc237a8f487b2cf0cff728576db7930
Chris Schnaars <chris@rewatch.com>
2025-03-08 08:32:34 -0500

    wip

--- src/audio/synth.js
+++ src/audio/synth.js
@@ -1,5 +1,27 @@
 import * as Tone from "tone";
 
-const synth = new Tone.Synth().toDestination();
+// create a tremolo and start it's LFO
+const tremolo = new Tone.Tremolo(9, 0.75).toDestination().start();
+
+const reverb = new Tone.Reverb(1).toDestination();
+const autoFilter = new Tone.AutoFilter("4n").toDestination().start();
+
+const synth = new Tone.Synth({
+  oscillator: {
+    type: "fmsine",
+    harmonicity: 0.5,
+    modulationType: "square",
+  },
+  envelope: {
+    attackCurve: "exponential",
+    attack: 0.05,
+    decay: 0.2,
+    sustain: 0.2,
+    release: 1.5,
+  },
+  portamento: 0.05,
+})
+  .connect(reverb)
+  .toDestination();
 
 export default synth;
